3b9cae6f87ec40d5d467dd97aa50760fd6702f57
lasha-codes <lashagiorgi420@gmail.com>
2024-03-30 12:12:22 -0700

    improved error handling and checking dup email from the database itself

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const { pool } = require('./dbConfig.js')
+const bcrypt = require('bcryptjs')
 
 require('dotenv').config()
 
@@ -25,7 +26,7 @@ app.get('/users/dashboard', (req, res) => {
   res.render('dashboard', { user: 'Lasha' })
 })
 
-app.post('/users/register', (req, res) => {
+app.post('/users/register', async (req, res) => {
   let { name, email, password, password2 } = req.body
 
   console.log({ name, email, password, password2 })
@@ -34,23 +35,32 @@ app.post('/users/register', (req, res) => {
 
   if (!name || !email || !password || !password2) {
     errors.push({ message: 'Please enter all fields' })
+    return console.log('All of the fields are required')
   }
 
   if (password.length < 6) {
     errors.push({ message: 'Password should be at least 6 characters' })
+    return window.alert('password must be at least 6 characters long')
   }
 
   if (password !== password2) {
     errors.push({ message: 'Passwords do not match' })
+    return console.log('both passwords must match each other')
   }
 
-  if (errors.length > 0) {
-    res.render('register', { errors })
-  }
+  let hashedPassword = await bcrypt.hash(password, 10)
+  console.log(hashedPassword)
+
+  pool.query(
+    `SELECT * FROM users WHERE email = $1`,
+    [email],
+    (err, results) => {
+      if (err) throw err
+      console.log(results.rows)
+    }
+  )
 })
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-// stopLine - 49:32
